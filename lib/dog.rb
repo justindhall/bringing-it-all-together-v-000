f946d3a7a5b1f929a5ee6e142d4591a8907b1832
Justin Hall <hall.justin.d@gmail.com>
2019-04-22 18:53:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,4 +72,12 @@ class Dog
     end.first
   end
 
+  def update
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ?, WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
 end
