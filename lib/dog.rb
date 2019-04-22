920e8c436fb4550ca5b4e733c61de595ea2f734d
Justin Hall <hall.justin.d@gmail.com>
2019-04-22 18:50:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,4 +55,11 @@ class Dog
     end.first
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
 end
