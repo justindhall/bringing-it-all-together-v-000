fd9ce0f0cf89fc9e0d2c01f11497ca7c8e7ef742
Justin Hall <hall.justin.d@gmail.com>
2019-04-22 18:49:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,9 @@ class Dog
       SELECT * FROM dogs WHERE id = ?
     SQL
 
-    DB[:conn].execute(sql, id)
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
